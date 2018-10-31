2f0e6d93ff98fe6f9936d3f0a5f821575cc47ffa
Jacob Schwartz Sørensen <jacobschwartzsoerensen@gmail.com>
2018-10-31 17:14:04 +0100

    Introduktion til Java koden

--- app/src/main/java/dk/nemprogrammering/android/listeapp/List.java
+++ app/src/main/java/dk/nemprogrammering/android/listeapp/List.java
@@ -2,12 +2,32 @@ package dk.nemprogrammering.android.listeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class List extends AppCompatActivity {
+
+    private static final String TAG = "List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String test = savedInstanceState.getString("test");
+
+        Log.d(TAG, "Value: " + test);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("test", "Dette er en test");
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
