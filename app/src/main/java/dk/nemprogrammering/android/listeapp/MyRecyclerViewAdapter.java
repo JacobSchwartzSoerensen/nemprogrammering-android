0e1753e16ed02985970e1ba4cabad32b36786c44
Jacob Schwartz Sørensen <jacobschwartzsoerensen@gmail.com>
2018-11-14 23:18:54 +0100

    Grafik

--- app/src/main/java/dk/nemprogrammering/android/listeapp/MyRecyclerViewAdapter.java
+++ app/src/main/java/dk/nemprogrammering/android/listeapp/MyRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package dk.nemprogrammering.android.listeapp;
 
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -11,9 +12,9 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView mTextView;
 
-        public MyViewHolder(TextView v)
+        public MyViewHolder(ConstraintLayout l, TextView v)
         {
-            super(v);
+            super(l);
             mTextView = v;
         }
     }
@@ -21,9 +22,10 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     @NonNull
     @Override
     public MyRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.list_element, parent, false);
+        ConstraintLayout layout = (ConstraintLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.list_element, parent, false);
+        TextView v = (TextView) layout.findViewById(R.id.list_text);
 
-        MyViewHolder vh = new MyViewHolder(v);
+        MyViewHolder vh = new MyViewHolder(layout, v);
         return vh;
     }
 
